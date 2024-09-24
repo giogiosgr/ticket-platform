d435645e144ff25769d9a6c40a78d458dc1ffc14
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-09-24 23:31:18 +0200

    Initial commit

---
+++ src/test/java/org/ticketplatform/java/TicketPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ticketplatform.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
