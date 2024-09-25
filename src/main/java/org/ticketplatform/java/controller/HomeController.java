cea2b480d8542b61850e8ef25f06d96974ab4053
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-09-25 23:54:00 +0200

    feat home e model operator

---
+++ src/main/java/org/ticketplatform/java/controller/HomeController.java
@@ -0,0 +1,20 @@
+package org.ticketplatform.java.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+	@GetMapping("/")
+	public String home(Model model) {
+
+		model.addAttribute("name", "Giovanni");
+
+		return "/pages/home";
+	}
+
+}
---
+++ src/main/java/org/ticketplatform/java/model/Operator.java
@@ -0,0 +1,98 @@
+package org.ticketplatform.java.model;
+
+import java.text.DecimalFormat;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Formula;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import jakarta.validation.constraints.*;
+
+@SuppressWarnings("unused")
+
+@Entity
+@Table(name = "operators")
+public class Operator {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	// viene usata la Bean Validation
+	
+	@NotNull
+	@NotEmpty
+	@Size(max=100)
+	@Column(name = "email", length=100, nullable = false, unique = true)
+	private String email;
+    
+	@Column(name = "status")
+	private boolean status;
+	
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+	
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
+	
+	// getters e setters
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+}
