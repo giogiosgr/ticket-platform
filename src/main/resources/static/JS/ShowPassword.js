0189a085d49b753afe0579801b3474bd9aaabbbc
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-10-11 19:57:09 +0200

    feat: script JS custom per mostrare le password

---
+++ src/main/resources/static/JS/ShowPassword.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// recupero elementi dei campi password dal DOM
+
+let passwordInput = document.getElementById("password");
+let showPasswordButton = document.getElementById("showPasswordButton");
+let validatePasswordInput = document.getElementById("validatePassword");
+let showValidatePasswordButton = document.getElementById("showValidatePasswordButton");
+
+// aggiunta eventi con funzioni per cambiare il type degli input delle password da "password" a "text", e viceversa
+
+showPasswordButton.addEventListener("mousedown", function() {
+
+	passwordInput.setAttribute("type", "text");
+
+});
+
+showPasswordButton.addEventListener("mouseup", function() {
+
+	passwordInput.setAttribute("type", "password");
+
+});
+
+showValidatePasswordButton.addEventListener("mousedown", function() {
+
+	validatePasswordInput.setAttribute("type", "text");
+
+});
+
+showValidatePasswordButton.addEventListener("mouseup", function() {
+
+	validatePasswordInput.setAttribute("type", "password");
+
+});
